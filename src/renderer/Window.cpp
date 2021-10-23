7fff829a6a3d96313b5fbbd1799b20b461866f7f
ernesto <ernesto.oliveira@inf.ufrgs.br>
2021-10-23 10:28:30 -0300

    Add comments to window initializer

--- src/renderer/Window.cpp
+++ src/renderer/Window.cpp
@@ -14,23 +14,22 @@ Window::Window(InputManager* input)
         fprintf(stderr, "ERROR: glfwInit() failed.\n");
         std::exit(EXIT_FAILURE);
     }
-
     glfwSetErrorCallback([](int error, const char* description) // lambda function
     {
         fprintf(stderr, "ERROR: GLFW: %s\n", description);
     });
-
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     glfwWindowHint(GLFW_SAMPLES, 4);
+    //glfw initialization
 
     GLFWmonitor* monitor = glfwGetPrimaryMonitor();
     const GLFWvidmode* screen = glfwGetVideoMode(monitor);
-
     this->window = glfwCreateWindow(screen->width, screen->height, "window", monitor, NULL);
     this->screenRatio = (float)screen->width/(float)screen->height;
+    //gets screen configuration and creates fullscreen window
 
     if (!this->window)
     {
@@ -39,31 +38,41 @@ Window::Window(InputManager* input)
         std::exit(EXIT_FAILURE);
     }
     glfwMakeContextCurrent(this->window);
-    glfwSetWindowUserPointer(this->window, input);
+    //creates glfw context
+
+    glfwSetWindowUserPointer(this->window, input); //sets input manager pointer, for handling callbacks
     glfwSetKeyCallback(this->window, [](GLFWwindow* window, int key, int scancode, int action, int mod)
     {
         auto input = (InputManager*)glfwGetWindowUserPointer( window );
         input->keyCallback(key, action, mod); 
     });
+    //lambda used for callbacks (calls input manager for handling)
+
     glfwSetCursorPosCallback(this->window, [](GLFWwindow* window, double xpos, double ypos)
     {
         auto input = (InputManager*)glfwGetWindowUserPointer( window );
         input->cursorCallback(xpos, ypos); 
     });
+    //lambda used for callbacks (calls input manager for handling)
+    
     double xpos,ypos;
     glfwGetCursorPos(this->window, &xpos, &ypos);
     input->setInitialCursorPos(xpos, ypos);
+    //sets initial position for mouse, since movement is calculated from mouse displacement
 
     glfwSetFramebufferSizeCallback(this->window, [](GLFWwindow* window, int width, int height)
     {
         glViewport(0, 0, width, height);
     });
+    //lambda used for callbacks, just updates ratio
 
 
     if (glfwRawMouseMotionSupported())
         glfwSetInputMode(this->window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+    //if possible, uses raw input, for better aim control
 
     glfwSetInputMode(this->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    //hides mouse cursor
 }
 
 Window::~Window()
