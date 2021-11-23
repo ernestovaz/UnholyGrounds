40f1a69153cdef2707c74cc005b3f305e546b7c8
ricco <ricco23052000@gmail.com>
2021-11-23 19:39:50 -0300

    Create collisions

---
+++ src/game/collisions.cpp
@@ -0,0 +1,14 @@
+bool PointPlaneCollision(){
+    //Arguments are a vec4 (point) and ?
+    return false;
+} 
+
+bool LineSphereCollision(){
+    //Arguments are a vec4 (vector) and ?
+    return false;
+}
+
+bool LinePlaneCollision(){
+    //Arguments are a vec4 (point) and ?
+    return false;
+}
