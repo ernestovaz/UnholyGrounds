847ab0a34aecab2b381ea6ade0017d8a25ce3220
ricco <riccovcs@terra.com.br>
2021-11-24 13:33:00 -0300

    Update Object to have a bounding box and define maxCoords and minCoords methods

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -3,13 +3,34 @@
 #include "Object.h"
 
 Object::Object()
+{//object's bounding box
+    for (int i = 0; i<= 7; i++){
+        boundingBox.push_back(glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
+    }
+}
+
+std::vector<float> Object::getMinCoords() //returns min (x,y,z) beteween bounding box's vertices 
 {
-    p1 = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
-    p2 = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
-    p3 = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
-    p4 = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
-    p5 = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
-    p6 = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
-    p7 = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
-    p8 = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
+    std::vector<float> minCoords = {1000.0, 1000.0, 1000.0};
+    for (int c = 0; c <= 2; c++){
+        for (int v = 0; v <= 7; v++){
+            if(boundingBox[v][c] < minCoords[c]){
+                minCoords[c] = boundingBox[v][c];
+            }
+        }
+    }
+    return minCoords;
+}
+
+std::vector<float> Object::getMaxCoords() //returns max (x,y,z) between bounding box's vertices
+{
+    std::vector<float> maxCoords = {0.0, 0.0, 0.0};
+    for (int c = 0; c <= 2; c++){
+        for (int v = 0; v <= 7; v++){
+            if(boundingBox[v][c] > maxCoords[c]){
+                maxCoords[c] = boundingBox[v][c];
+            }
+        }
+    }
+    return maxCoords;
 }
--- src/game/Object.h
+++ src/game/Object.h
@@ -1,16 +1,12 @@
 #pragma once
 
 #include <glm/vec4.hpp>
+#include <vector>
 
 class Object{
     public:
-        glm::vec4 p1;
-        glm::vec4 p2;
-        glm::vec4 p3;
-        glm::vec4 p4;
-        glm::vec4 p5;
-        glm::vec4 p6;
-        glm::vec4 p7;
-        glm::vec4 p8;
+        std::vector<glm::vec4> boundingBox;
         Object();
+        std::vector<float> getMinCoords();
+        std::vector<float> getMaxCoords();
 };
