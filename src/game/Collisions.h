9b6539d7cfbdc0470ee1aec4b4c898c4f49917d4
ernesto <evoliveira@inf.ufrgs.br>
2021-12-17 17:56:42 -0300

    Implement calculation of ray-sphere intersection

--- src/game/Collisions.h
+++ src/game/Collisions.h
@@ -8,5 +8,6 @@ bool pointBoundingBoxCollision(glm::vec4 point,BoundingBox box);
 bool lineBoundingBoxCollision(glm::vec4 position, glm::vec4 vector, BoundingBox box);
 bool pointLineSegment(glm::vec4 point, glm::vec4 p1, glm::vec4 p2);
 bool pointSquare(glm::vec4 point, std::vector<glm::vec4> squareVertices);
+bool raySphere(glm::vec4 rayOrigin, glm::vec4 rayDirection, glm::vec4 sphereOrigin, glm::vec4 sphereRadius, float maxDistance = 0);
 
 std::vector<float> quadraticRoot(float a, float b, float c);
