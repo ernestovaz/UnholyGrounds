2dbc249ddb44cc46a744be0b2b7819a3b00e6d5e
I587281 <ernesto.oliveira@sap.com>
2023-07-29 13:05:16 -0300

    Configure for macOS compatibility

--- src/renderer/Window.cpp
+++ src/renderer/Window.cpp
@@ -20,7 +20,8 @@ Window::Window()
         fprintf(stderr, "ERROR: GLFW: %s\n", description);
     });
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
